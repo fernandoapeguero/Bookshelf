6344ae95f078d15cd21015a48ce9e908bb6e4957
snowbreaker <snowbreaker@Ubuntu.myguest.virtualbox.org>
2023-03-12 21:26:27 -0400

    Add functionality for opening and closing modal

--- Bookshelf/script.js
+++ Bookshelf/script.js
@@ -1,3 +1,22 @@
 function logMessage() {
   console.log("Yeah this is nice");
 }
+
+// Shows Modal on button click
+
+function showModal() {
+  if (modal.style.display == "none") {
+    modal.style.display = "grid";
+  }
+}
+
+//  Hides Modal
+const modal = document.querySelector(".form_container");
+
+modal.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
